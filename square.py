d41fcd790086a242cebcea6cd996877888fad169
stephen-nene <stevekid705@mail.com>
2024-02-13 07:17:01 +0300

    fixed users

--- square.py
+++ square.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" Module for square class"""
 
-
-class Square():
-    """ Square class """
+class square():
+    
     width = 0
     height = 0
 
+    
     def __init__(self, *args, **kwargs):
-        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,18 +14,15 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def permiter_of_my_square(self):
-        """ Perimeter of my square """
+    def PermiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
-
 if __name__ == "__main__":
-    """ Create a square object """
-    s = Square(width=12, height=9)
+
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiter_of_my_square())
+    print(s.PermiterOfMySquare())
